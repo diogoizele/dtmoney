57aab428a58ea49b11fd0d65eeb8677fcc9060b4
Diogo Izele <diogogabrielizele@gmail.com>
2022-06-18 13:35:33 -0300

    feat(server): creates a fake server to provider data mocks in application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { createServer } from "miragejs";
+
 import { Header } from "components/header/header";
 import { GlobalStyle } from "styles/global";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Desenvolvimento de website",
+          value: 12000,
+          category: "Desenvolvimento",
+          date: "23/03/2022",
+        },
+        {
+          id: 2,
+          title: "Curso  de ReactJS",
+          value: 2000,
+          category: "Lazer",
+          date: "23/03/2022",
+        },
+      ];
+    });
+  },
+});
 
 export const App = () => {
   return (
