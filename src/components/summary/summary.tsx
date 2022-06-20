add2d83045206a3f6a4f71d91ad39355e56e8195
Diogo Izele <diogogabrielizele@gmail.com>
2022-06-20 18:59:51 -0300

    refactor(summary): shows real values about transactions in summary

--- src/components/summary/summary.tsx
+++ src/components/summary/summary.tsx
@@ -1,10 +1,33 @@
 import { Container } from "./summary.styles";
-
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
 
+import { formatAmount } from "utils/formatCurrency";
+import { useTransactions } from "hooks/useTransactions";
+
 export const Summary = () => {
+  const { transactions } = useTransactions();
+
+  const summary = transactions.reduce(
+    (acc, curr) => {
+      if (curr.type === "deposit") {
+        acc.deposit += curr.amount;
+        acc.total += curr.amount;
+      } else {
+        acc.withdraw += curr.amount;
+        acc.total -= curr.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposit: 0,
+      withdraw: 0,
+      total: 0,
+    }
+  );
+
   return (
     <Container>
       <div>
@@ -12,22 +35,21 @@ export const Summary = () => {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>{formatAmount(summary.deposit)}</strong>
       </div>
-
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>- {formatAmount(summary.withdraw)}</strong>
       </div>
       <div className="hightlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>{formatAmount(summary.total)}</strong>
       </div>
     </Container>
   );
